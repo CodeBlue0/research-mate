c8c6b86706e7f85a86d2006e9ec3d47cccd92ca5
CodeBlue0 <piri007@cau.ac.kr>
2026-01-13 21:32:46 +0900

    fix: 구글 로그인 시 에러 해결

--- src/app/auth/actions.ts
+++ src/app/auth/actions.ts
@@ -45,10 +45,24 @@ export async function signup(formData: FormData) {
 
 export async function signInWithGoogle() {
     const supabase = await createClient()
+
+    const getUrl = () => {
+        let url = process.env.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+            process.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+            process.env.VERCEL_URL ?? // System env var on Vercel
+            'http://localhost:3000/'
+
+        // Make sure to include `https://` when not localhost.
+        url = url.includes('http') ? url : `https://${url}`
+        // Make sure to include a trailing slash
+        url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
+        return url
+    }
+
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-            redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'}/auth/callback`,
+            redirectTo: `${getUrl()}auth/callback`,
         },
     })
 
